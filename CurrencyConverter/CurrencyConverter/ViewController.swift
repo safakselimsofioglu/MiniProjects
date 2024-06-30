be1da29ce042c8a589ccba31ab380e1b7f2418a4
safakselimsofioglu <154444832+safakselimsofioglu@users.noreply.github.com>
2024-06-30 13:07:14 +0300

    Developer with ExchangeRate-API

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Şafak Selim Sofioğlu on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 3) Parsing & JSON Serialization
+        
+        // 1) Request & Session
+        let apiLink = "https://v6.exchangerate-api.com/v6/7950c4a6a4cf5db9588b8785/latest/EUR"
+        let url = URL(string: apiLink)
+        let session = URLSession.shared
+        
+        //Closure
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                // 2) Response & Data
+                if data != nil {
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        
+                        //ASYNC
+                        DispatchQueue.main.async {
+                            if let rates = jsonResponse["conversion_rates"] as? [String : Any] {
+                                if let cad = rates["CAD"] as? Double {
+                                    self.cadLabel.text = "CAD: \(cad)"
+                                }
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF: \(chf)"
+                                }
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gbpLabel.text = "GBP: \(gbp)"
+                                }
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text = "JPY: \(jpy)"
+                                }
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text = "USD: \(usd)"
+                                }
+                                if let turkish = rates["TRY"] as? Double {
+                                    self.tryLabel.text = "TRY: \(turkish)"
+                                }
+                            }
+                        }
+                        
+                    } catch {
+                        print("error")
+                    }
+                }
+            }
+        }
+        task.resume() //Başlatır
+    }
+    
+}
+
